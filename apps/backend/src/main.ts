73524ed7bba7e074c9b29484e5e72522764c1bc7
Hairum Qureshi <159098695+Hairum-Qureshi@users.noreply.github.com>
2026-06-15 11:19:15 -0400

    replaced process.env with configService

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
     credentials: true,
   });
   
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('Nest.js Server successfully started');
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') ?? 3000;
+  await app.listen(PORT);
+
+  console.log(`Nest.js Server successfully started on port ${PORT}`);
 }
 bootstrap();
