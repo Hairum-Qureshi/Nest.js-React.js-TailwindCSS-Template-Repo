3d8f4dc929fc821acd115f79d3265953a96ee1d1
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-15 22:23:38 -0400

    removed unnecessary dotenv imports

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -12,9 +9,9 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
-  
+
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT') ?? 3000;
+  const PORT = configService.get<number>('PORT') ?? 4000;
   await app.listen(PORT);
 
   console.log(`Nest.js Server successfully started on port ${PORT}!`);
