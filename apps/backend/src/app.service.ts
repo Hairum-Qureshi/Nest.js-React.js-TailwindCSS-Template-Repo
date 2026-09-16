79eb6345b1c71382c01c64823c81bc8117aa79bf
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-15 22:19:08 -0400

    changed getHello method to return GreetingResponse interface object that's also imported from shared repo

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import type { GreetingResponse } from '@repo/shared';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): GreetingResponse {
+    return {
+      greeting: 'Hello from Nest.js!',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
