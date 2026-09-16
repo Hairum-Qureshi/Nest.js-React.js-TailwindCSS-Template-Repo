7f143256d33f285817ac6c8a9052dfd6bca7f6a2
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-15 22:18:40 -0400

    replaced string type with custom GreetingResponse interface from shared folder

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { GreetingResponse } from '@repo/shared';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): GreetingResponse {
     return this.appService.getHello();
   }
 }
